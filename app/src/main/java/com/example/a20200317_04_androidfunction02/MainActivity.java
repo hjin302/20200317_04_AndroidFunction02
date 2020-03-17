911eb219f7830a1e7a8ebd3ea5698ec0ee7914c1
hjin302 <enro302@naver.com>
2020-03-17 17:16:29 +0900

    전화걸기 직전 화면으로 이동

--- app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
+++ app/src/main/java/com/example/a20200317_04_androidfunction02/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.a20200317_04_androidfunction02;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.a20200317_04_androidfunction02.databinding.ActivityMainBinding;
 
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
+        binding.dialBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                다른 화면으로 이동 : Intent
+//                => 우리가 만든게 아니라, 안드로이드가 제공하는 화면으로 이동.
+//                => 전화걸기 화면!
+
+//                어느 화면으로 갈지 => 안드로이드가 요구하는 양식으로 작성: Uri
+                Uri uri = Uri.parse("tel: 01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+
+            }
+        });
     }
 }
